1f300015d7b9dcaad89eb24a8f1a4e821b73f7fe
Nikita_Kucherenko <ext_nkucherenko@rencredit.ru>
2025-12-05 01:10:31 +0300

    init project files

---
+++ src/main/java/dev/redradish/investai/AiInvestBotApplication.java
@@ -0,0 +1,13 @@
+package dev.redradish.investai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInvestBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiInvestBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/redradish/investai/AiInvestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.redradish.investai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInvestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
